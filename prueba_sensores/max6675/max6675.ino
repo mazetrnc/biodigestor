f6ab43883abf91f03175f02817a3e44f49e1126b
Maze <129135957+mazetrnc@users.noreply.github.com>
2025-09-02 22:09:42 -0500

    Add files via upload

---
+++ prueba_sensores/max6675/max6675.ino
@@ -0,0 +1,32 @@
+#include "MAX6675.h"
+const int dataPin = 19;
+const int clockPin = 18;
+const int selectPin = 5;
+MAX6675 thermoCouple(selectPin, dataPin, clockPin);
+uint32_t start, stop;
+void setup() {
+  Serial.begin(115200);
+  SPI.begin();
+  thermoCouple.begin();
+  thermoCouple.setSPIspeed(4000000);
+}
+
+
+void loop() {
+  int status = thermoCouple.read();
+  float temp = thermoCouple.getTemperature();
+  //thermoCouple.setOffset(-10.0);
+  if (status == 0) {
+  Serial.print("temp: ");
+  Serial.println(temp);
+  }
+  else if (status == 4) {
+    Serial.println("Revisar termopar");
+  }
+
+  else if (status == 129) {
+    Serial.println("Sin comunicacion");
+  }
+
+  delay(500);
+}
